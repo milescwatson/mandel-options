08af314375652cad1be99b2f8d20ed7ff2453a58
Miles Watson <miles@milescwatson.com>
2020-08-02 21:46:08 -0700

    add parse strategy

--- server/mail.js
+++ server/mail.js
@@ -23,12 +23,35 @@ mailin.on('message', function (connection, data, content) {
 var parseMessage = function(data){
     const messageSubject = data.subject;
     const messageBody = data.html;
-
     //get ticker
     var ticker = messageBody.match("......</span></a> alert");
     ticker = ticker[0].split("<")[0].trim();
 
-	
+    var parseStrategy = function(messageBody){
+    	var target = '$$',
+    			targetIterator = 0,
+    			accumulator = [],
+    			accumulate = false;
+
+    	for (var i = 0; i < messageBody.length; i++) {
+    		if(messageBody[i] === '$' && messageBody[i+1] === '$'){
+    			accumulate = true;
+    		}
+    		if(accumulate){
+    			if(messageBody.substr(i,i+4) === "</td>"){
+    				accumulate = false;
+    			}else{
+    				accumulator.push(messageBody[i]);
+    			}
+    		}
+
+    	}
+    	return(accumulator.join(""));
+    }
+    var strategyTitle = parseStrategy(messageBody)
+
+    console.log('strategyTitle = ', strategyTitle)
+
     var strategyDirection = '';
 }
 
